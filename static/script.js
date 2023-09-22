8a18acddf2d17f00908048ffeeab73adfad0d4ec
julian carreiro <julian.vt@gmail.com>
2023-09-22 19:21:20 -0300

    script js refatorado é o padrao

--- static/script.js
+++ static/script.js
@@ -1,210 +1,191 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("DOMContentLoaded fired");
-    const waterButton = document.getElementById("water-button");
-    const sensorContainer = document.getElementById("sensor-container");
-    const darkModeToggle = document.getElementById("dark-mode-toggle");
-    const container = document.querySelector(".container");
-    const body = document.body;
-    const preferredMode = localStorage.getItem("preferredMode");
+class GardenApp {
+    constructor() {
+        this.waterButton = document.getElementById("water-button");
+        this.sensorContainer = document.getElementById("sensor-container");
+        this.darkModeToggle = document.getElementById("dark-mode-toggle");
+        this.container = document.querySelector(".container");
+        this.body = document.body;
+        this.preferredMode = localStorage.getItem("preferredMode") || "light";
+        this.preferredLanguage = "portuguese"; // Idioma padrão
+        this.translatedText = {
+            // Traduções para o inglês
+            english: {
+                sensors:"Sensors",
+                garden: "Garden",
+                darkMode: "Dark Mode",
+                lightMode: "Light Mode",
+                manualWatering: "Manual Watering",
+                sensorSection: "Air and Soil Sensors",
+                airTemp: "Air Temperature",
+                soilTemp: "Soil Temperature",
+                ph: "Soil ph",
+                airHumidity: "Air Humidity",
+                soilMoisture: "Soil Moisture",
+                electricitySection: "Electricity and Reservoirs",
+                electricalConsumption: "Electrical Consumption",
+                reservoirl1: "Reservoir Level 1",
+                reservoirl2: "Reservoir Level 2",
+                environmentalSensors: "Environmental Sensors",
+                co2: "CO2 Level",
+                light: "Light Level",
+            },
+                // Traduções para o português
+                portuguese: {
+                sensors:"Sensores",
+                garden: "Jardim",
+                darkMode: "Modo Escuro",
+                lightMode: "Modo Claro",
+                manualWatering: "Regar Manualmente",
+                sensorSection: "Sensores de Ar e Solo",
+                airTemp: "Temperatura do Ar",
+                soilTemp: "Temperatura do Solo",
+                ph: "ph do Solo",
+                airHumidity: "Umidade do Ar",
+                soilMoisture: "Umidade do Solo",
+                electricitySection: "Eletricidade e Reservatórios",
+                electricalConsumption: "Consumo de Eletricidade",
+                reservoirl1: "Nível do Reservatório 1",
+                reservoirl2: "Nível do Reservatório 2",
+                environmentalSensors: "Sensores Ambientais",
+                co2: "Nível de CO2",
+                light: "Nível de Luz",
+                },
+        };
 
-    // Defina o idioma preferido inicialmente (por exemplo, inglês)
-    let preferredLanguage = "portuguese"; // Defina o idioma padrão
+        this.init();
+    }
 
-    // Objeto com traduções
-    const translatedText = {
-        // Traduções para o inglês
-        english: {
-            sensors:"Sensors",
-            garden: "Garden",
-            darkMode: "Dark Mode",
-            lightMode: "Light Mode",
-            manualWatering: "Manual Watering",
-            sensorSection: "Air and Soil Sensors",
-            airTemp: "Air Temperature",
-            soilTemp: "Soil Temperature",
-            ph: "Soil ph",
-            airHumidity: "Air Humidity",
-            soilMoisture: "Soil Moisture",
-            electricitySection: "Electricity and Reservoirs",
-            electricalConsumption: "Electrical Consumption",
-            reservoirl1: "Reservoir Level 1",
-            reservoirl2: "Reservoir Level 2",
-            environmentalSensors: "Environmental Sensors",
-            co2: "CO2 Level",
-            light: "Light Level",
-        },
-        // Traduções para o português
-        portuguese: {
-            sensors:"Sensores",
-            garden: "Jardim",
-            darkMode: "Modo Escuro",
-            lightMode: "Modo Claro",
-            manualWatering: "Regar Manualmente",
-            sensorSection: "Sensores de Ar e Solo",
-            airTemp: "Temperatura do Ar",
-            soilTemp: "Temperatura do Solo",
-            ph: "ph do Solo",
-            airHumidity: "Umidade do Ar",
-            soilMoisture: "Umidade do Solo",
-            electricitySection: "Eletricidade e Reservatórios",
-            electricalConsumption: "Consumo de Eletricidade",
-            reservoirl1: "Nível do Reservatório 1",
-            reservoirl2: "Nível do Reservatório 2",
-            environmentalSensors: "Sensores Ambientais",
-            co2: "Nível de CO2",
-            light: "Nível de Luz",
-        },
-    };
+    init() {
+        this.addEventListeners();
+        this.setMode(this.preferredMode);
+        this.fetchAndUpdateSensorData();
+        setInterval(() => this.fetchAndUpdateSensorData(), 1000);
+    }
 
-    function setMode(mode) {
+    addEventListeners() {
+        this.darkModeToggle.addEventListener("click", () => this.toggleDarkMode());
+        document.getElementById("language-switch-btn").addEventListener("click", () => this.toggleLanguage());
+        this.waterButton.addEventListener("click", () => this.waterPlant());
+        document.querySelector(".sensor-toggle").addEventListener("click", () => this.toggleSensorList());
+    }
+
+    setMode(mode) {
         if (mode === "dark") {
-            container.classList.add("dark-mode");
-            body.classList.add("dark-mode");
-            darkModeToggle.innerText = translatedText[preferredLanguage].lightMode;
+            this.container.classList.add("dark-mode");
+            this.body.classList.add("dark-mode");
+            this.darkModeToggle.innerText = this.translatedText[this.preferredLanguage].lightMode;
         } else {
-            container.classList.remove("dark-mode");
-            body.classList.remove("dark-mode");
-            darkModeToggle.innerText = translatedText[preferredLanguage].darkMode;
+            this.container.classList.remove("dark-mode");
+            this.body.classList.remove("dark-mode");
+            this.darkModeToggle.innerText = this.translatedText[this.preferredLanguage].darkMode;
         }
-        // Salvar a preferência do usuário
         localStorage.setItem("preferredMode", mode);
     }
 
-    // Event listener para o botão de alternar o modo escuro
-    darkModeToggle.addEventListener("click", function () {
-        if (body.classList.contains("dark-mode")) {
-            setMode("light");
-        } else {
-            setMode("dark");
-        }
-    });
-
-    // Definir o modo inicial com base na preferência do usuário ou na preferência do sistema
-    if (preferredMode === "dark" || (preferredMode === null && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
-        setMode("dark");
-    } else {
-        setMode("light");
+    toggleDarkMode() {
+        this.setMode(this.body.classList.contains("dark-mode") ? "light" : "dark");
     }
 
-    function fetchAndUpdateSensorData() {
+    toggleLanguage() {
+        this.preferredLanguage = this.preferredLanguage === "english" ? "portuguese" : "english";
+        this.updateTexts();
+    }
+
+    updateTexts() {
+        // Atualize os textos na página com base no novo idioma
+        this.darkModeToggle.innerText = this.translatedText[this.preferredLanguage].darkMode;
+        this.waterButton.innerText = this.translatedText[this.preferredLanguage].manualWatering;
+        // ... (atualize outros elementos conforme necessário)
+        document.getElementById("language-switch-btn").innerText =
+            this.preferredLanguage === "english" ? "Português" : "English";
+    }
+
+    fetchAndUpdateSensorData() {
         fetch('/sensor-data')
             .then((response) => response.json())
             .then((data) => {
-                // Atualizar os dados dos sensores com base nos dados recebidos da API do Flask
-                updateSensorData(data);
+                this.updateSensorData(data);
             });
     }
 
-    // Função para atualizar os dados dos sensores na página
-    function updateSensorData(data) {
-        // Atualizar os dados do sensor na página
-        var sensorData = `
-            
+    updateSensorData(data) {
+        // Atualize os dados dos sensores na página
+        const sensorData = `
             <div class="sensor-item">
                 <i class="material-icons">thermostat</i>
-                <span>${translatedText[preferredLanguage].airTemp}: ${data.air_temp}</span>
+                <span>${this.translatedText[this.preferredLanguage].airTemp}: ${data.air_temp}</span>
             </div>
             <div class="sensor-item">
                 <i class="material-icons">thermostat</i>
-                <span>${translatedText[preferredLanguage].soilTemp}: ${data.soil_temp}</span>
+                <span>${this.translatedText[this.preferredLanguage].soilTemp}: ${data.soil_temp}</span>
             </div>
             <div class="sensor-item">
                 <i class="material-icons">invert_colors</i>
-                <span>${translatedText[preferredLanguage].ph}: ${data.ph}</span>
+                <span>${this.translatedText[this.preferredLanguage].ph}: ${data.ph}</span>
             </div>
             <div class="sensor-item">
                 <i class="material-icons">wb_sunny</i>
-                <span>${translatedText[preferredLanguage].airHumidity}: ${data.air_humidity}</span>
+                <span>${this.translatedText[this.preferredLanguage].airHumidity}: ${data.air_humidity}</span>
             </div>
             <div class="sensor-item">
                 <i class="material-icons">opacity</i>
-                <span>${translatedText[preferredLanguage].soilMoisture}: ${data.soil_moisture}</span>
+                <span>${this.translatedText[this.preferredLanguage].soilMoisture}: ${data.soil_moisture}</span>
             </div>
 
-            <h2>${translatedText[preferredLanguage].electricitySection}</h2>
+            <h2>${this.translatedText[this.preferredLanguage].electricitySection}</h2>
             <div class="sensor-item">
                 <i class="material-icons">power</i>
-                <span>${translatedText[preferredLanguage].electricalConsumption}: ${data.electrical_consumption}</span>
+                <span>${this.translatedText[this.preferredLanguage].electricalConsumption}: ${data.electrical_consumption}</span>
             </div>
             <div class="sensor-item">
                 <i class="material-icons">local_drink</i>
-                <span>${translatedText[preferredLanguage].reservoirl1}: ${data.reservoir_l1}</span>
+                <span>${this.translatedText[this.preferredLanguage].reservoirl1}: ${data.reservoir_l1}</span>
             </div>
             <div class="sensor-item">
                 <i class="material-icons">local_drink</i>
-                <span>${translatedText[preferredLanguage].reservoirl2}: ${data.reservoir_l2}</span>
+                <span>${this.translatedText[this.preferredLanguage].reservoirl2}: ${data.reservoir_l2}</span>
             </div>
 
-            <h2>${translatedText[preferredLanguage].environmentalSensors}</h2>
+            <h2>${this.translatedText[this.preferredLanguage].environmentalSensors}</h2>
             <div class="sensor-item">
                 <i class="material-icons">cloud</i>
-                <span>${translatedText[preferredLanguage].co2}: ${data.co2}</span>
+                <span>${this.translatedText[this.preferredLanguage].co2}: ${data.co2}</span>
             </div>
             
             <div class="sensor-item">
                 <i class="material-icons">wb_incandescent</i>
-                <span>${translatedText[preferredLanguage].light}: ${data.light}</span>
-            </div>
-        `;
-         // Verifique se o sensorContainer está oculto antes de atualizá-lo
-         if (!sensorContainer.classList.contains("hidden")) {
-            sensorContainer.innerHTML = sensorData;}
+                <span>${this.translatedText[this.preferredLanguage].light}: ${data.light}</span>
+            </div>`
+        ;
+    if (!this.sensorContainer.classList.contains("hidden")) {
+        this.sensorContainer.innerHTML = sensorData;
+        }
     }
 
-    // Event listener para o título "Sensores"
-    const sensorTitle = document.querySelector(".sensor-toggle");
-    const sensorList = document.getElementById("sensor-list");
-    const sensorToggleIcon = document.getElementById("sensor-toggle-icon");
+    toggleSensorList() {
+        const sensorList = document.getElementById("sensor-list");
+        const sensorToggleIcon = document.getElementById("sensor-toggle-icon");
+        const sensorTitle = document.querySelector(".sensor-toggle");
 
-    sensorTitle.addEventListener("click", function () {
-        // Toggle para ocultar ou mostrar a lista de sensores
-        console.log("Sensor Title clicked");
         sensorList.classList.toggle("hidden");
-        
-        // Gire a seta do ícone
         sensorToggleIcon.classList.toggle("rotate-icon");
-        
-        // Adicione ou remova a classe "clicked" para a mudança de estilo
         sensorTitle.classList.toggle("clicked");
-    });
-    // Buscar dados do sensor inicialmente quando a página carrega
-    fetchAndUpdateSensorData();
+    }
 
-    // Configurar um temporizador para buscar dados do sensor periodicamente (por exemplo, a cada 5 segundos)
-    setInterval(fetchAndUpdateSensorData, 5000);
-
-    // Event listener para alternar entre inglês e português
-    const languageSwitchBtn = document.getElementById("language-switch-btn");
-    languageSwitchBtn.addEventListener("click", function () {
-        // Alternar entre inglês e português
-        preferredLanguage = preferredLanguage === "english" ? "portuguese" : "english";
-
-        // Atualize os textos na página com base no novo idioma
-        darkModeToggle.innerText = translatedText[preferredLanguage].darkMode;
-        waterButton.innerText = translatedText[preferredLanguage].manualWatering;
-        garden.innerText = translatedText[preferredLanguage].garden;
-        sensors.innerText = translatedText[preferredLanguage].sensors;
-
-
-        // Atualize o texto do botão de alternância de idioma
-        languageSwitchBtn.innerText = preferredLanguage === "english" ? "Português" : "English";
-    });
-    
-    
-    // Event listener para o botão de regar a planta
-    waterButton.addEventListener("click", function () {
-        // Enviar uma solicitação POST para a rota /water-plant da API
+    waterPlant() {
         fetch("/water-plant", {
             method: "POST",
         })
         .then((response) => response.json())
         .then((data) => {
-            // Exibir um alerta com a mensagem da API
             alert(data.message);
         })
         .catch((error) => {
             console.error("Erro ao enviar a solicitação:", error);
         });
-    });
+    }
+}
 
+document.addEventListener("DOMContentLoaded", () => {
+    new GardenApp();
 });
