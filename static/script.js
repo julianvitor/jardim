98b50a96a314645cd5f5c9c26e518d8bf28d3619
Julian Vitor <julian.vtr@gmail.com>
2023-12-13 16:46:50 -0300

    separando os dados da dashboard

--- static/script.js
+++ static/script.js
@@ -1,32 +1,18 @@
 class GardenApp {
     constructor() {
+        // Elementos do DOM
         this.waterButton = document.getElementById("water-button");
+
         this.sensorContainer = document.getElementById("sensor-container");
+
         this.darkModeToggle = document.getElementById("dark-mode-toggle");
         this.container = document.querySelector(".container");
         this.body = document.body;
-        this.preferredMode = localStorage.getItem("preferredMode") || "light";
-        this.text = {
-                sensors:"Sensors",
-                garden: "Garden",
-                darkMode: "Dark Mode",
-                lightMode: "Light Mode",
-                manualWatering: "Manual Watering",
-                sensorSection: "Air and Soil Sensors",
-                airTemp: "Air Temperature",
-                soilTemp: "Soil Temperature",
-                ph: "Soil ph",
-                airHumidity: "Air Humidity",
-                soilMoisture: "Soil Moisture",
-                electricitySection: "Electricity and Reservoirs",
-                electricalConsumption: "Electrical Consumption",
-                reservoirl1: "Reservoir Level 1",
-                reservoirl2: "Reservoir Level 2",
-                environmentalSensors: "Environmental Sensors",
-                co2: "CO2 Level",
-                light: "Light Level",
-        };
 
+        // Modo preferido
+        this.preferredMode = localStorage.getItem("preferredMode") || "light";
+
+        // Inicialização
         this.init();
     }
 
@@ -38,102 +24,116 @@ class GardenApp {
     }
 
     addEventListeners() {
+        // Event Listeners
         this.darkModeToggle.addEventListener("click", () => this.toggleDarkMode());
         this.waterButton.addEventListener("click", () => this.waterPlant());
-        document.querySelector(".sensor-toggle").addEventListener("click", () => this.toggleSensorList());
+        document.getElementById("sensors").addEventListener("click", () => this.toggleSensorList("sensor-list", "sensor-toggle-icon"));
     }
 
     setMode(mode) {
+        // Configuração do Modo
         if (mode === "dark") {
             this.container.classList.add("dark-mode");
             this.body.classList.add("dark-mode");
-            this.darkModeToggle.innerText = this.text.lightMode;
+            this.darkModeToggle.innerText = "Light Mode";
         } else {
             this.container.classList.remove("dark-mode");
             this.body.classList.remove("dark-mode");
-            this.darkModeToggle.innerText = this.text.darkMode;
+            this.darkModeToggle.innerText = "Dark Mode";
         }
+
+        // Armazenamento local
         localStorage.setItem("preferredMode", mode);
     }
 
     toggleDarkMode() {
+        // Alternância de Modo Escuro
         this.setMode(this.body.classList.contains("dark-mode") ? "light" : "dark");
     }
 
-
     fetchAndUpdateSensorData() {
+        // Busca e Atualização de Dados do Sensor
         fetch('/sensor-data')
             .then((response) => response.json())
             .then((data) => {
                 this.updateSensorData(data);
+                this.updateSensorERData(data);
             });
     }
 
     updateSensorData(data) {
-        // Atualize os dados dos sensores na página
+        // Atualização dos Dados dos Sensores
         const sensorData = `
             <div class="sensor-item">
                 <i class="material-icons">thermostat</i>
-                <span>${this.text.airTemp}: ${data.air_temp}</span>
+                <span>Air Temperature: ${data.air_temp}</span>
             </div>
             <div class="sensor-item">
                 <i class="material-icons">thermostat</i>
-                <span>${this.text.soilTemp}: ${data.soil_temp}</span>
+                <span>Soil Temperature: ${data.soil_temp}</span>
             </div>
             <div class="sensor-item">
                 <i class="material-icons">invert_colors</i>
-                <span>${this.text.ph}: ${data.ph}</span>
+                <span>Soil pH: ${data.ph}</span>
             </div>
             <div class="sensor-item">
                 <i class="material-icons">wb_sunny</i>
-                <span>${this.text.airHumidity}: ${data.air_humidity}</span>
+                <span>Air Humidity: ${data.air_humidity}</span>
             </div>
             <div class="sensor-item">
                 <i class="material-icons">opacity</i>
-                <span>${this.text.soilMoisture}: ${data.soil_moisture}</span>
+                <span>Soil Moisture: ${data.soil_moisture}</span>
             </div>
 
-            <h2>${this.text.electricitySection}</h2>
+            <h2>Electricity and Reservoirs</h2>
             <div class="sensor-item">
                 <i class="material-icons">power</i>
-                <span>${this.text.electricalConsumption}: ${data.electrical_consumption}</span>
+                <span>Electrical Consumption: ${data.electrical_consumption}</span>
             </div>
             <div class="sensor-item">
                 <i class="material-icons">local_drink</i>
-                <span>${this.text.reservoirl1}: ${data.reservoir_l1}</span>
+                <span>Reservoir Level 1: ${data.reservoir_l1}</span>
             </div>
             <div class="sensor-item">
                 <i class="material-icons">local_drink</i>
-                <span>${this.text.reservoirl2}: ${data.reservoir_l2}</span>
+                <span>Reservoir Level 2: ${data.reservoir_l2}</span>
             </div>
 
-            <h2>${this.text.environmentalSensors}</h2>
+            <h2>Environmental Sensors</h2>
             <div class="sensor-item">
                 <i class="material-icons">cloud</i>
-                <span>${this.text.co2}: ${data.co2}</span>
+                <span>CO2 Level: ${data.co2}</span>
             </div>
             
             <div class="sensor-item">
                 <i class="material-icons">wb_incandescent</i>
-                <span>${this.text.light}: ${data.light}</span>
-            </div>`
-        ;
-    if (!this.sensorContainer.classList.contains("hidden")) {
-        this.sensorContainer.innerHTML = sensorData;
+                <span>Light Level: ${data.light}</span>
+            </div>
+        `;
+
+        if (!this.sensorContainer.classList.contains("hidden")) {
+            this.sensorContainer.innerHTML = sensorData;
         }
     }
 
-    toggleSensorList() {
-        const sensorList = document.getElementById("sensor-list");
-        const sensorToggleIcon = document.getElementById("sensor-toggle-icon");
-        const sensorTitle = document.querySelector(".sensor-toggle");
-
+    toggleSensorList(sensorListId, toggleIconId, sensorContainer) {
+        // Alternância de Lista de Sensores
+        const sensorList = document.getElementById(sensorListId);
+        const toggleIcon = document.getElementById(toggleIconId);
+        const sensorTitle = document.getElementById(sensorListId.replace("-list", ""));
+    
         sensorList.classList.toggle("hidden");
-        sensorToggleIcon.classList.toggle("rotate-icon");
+        toggleIcon.classList.toggle("rotate-icon");
         sensorTitle.classList.toggle("clicked");
+    
+        // Ocultar ou Exibir o Conteúdo do Contêiner de Sensores Específico
+        if (sensorContainer && sensorContainer.classList) {
+            sensorContainer.classList.toggle("hidden");
+        }
     }
 
     waterPlant() {
+        // Irrigação da Planta
         fetch("/water-plant", {
             method: "POST",
         })
