dda98709e4a13f31397102dc32d81b627de653e5
Julian Vitor <julian.vtr@gmail.com>
2023-12-14 16:48:09 -0300

    aceitando todos os ips no uvicorn

--- startup_back.sh
+++ startup_back.sh
@@ -12,7 +12,7 @@ cd app/jardim
 pip install -r requirements.txt
 
 # Inicie o aplicativo fastapi
-uvicorn gateway:app --reload --workers 1 --port 8000
+uvicorn gateway:app --reload --workers 1 --host 0.0.0.0 --port 8000
 
 
 
